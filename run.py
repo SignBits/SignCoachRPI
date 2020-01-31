a37023180d0c07b2225a2cedcabdfde91dd71759
Zhicheng <s1762811@sms.ed.ac.uk>
2020-01-31 21:12:28 +0000

    set the default environment to production

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app
 
-config_name = os.getenv('FLASK_ENV', 'dev')
+config_name = os.getenv('FLASK_ENV', 'prod')
 app = create_app(config_name)
 
 if __name__ == '__main__':
